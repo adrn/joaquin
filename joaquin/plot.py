cc8315c2569c05810bdac6e91a9d6744be5e253f
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-03-30 22:09:50 -0400

    move two panel hr/cmd plot into module

--- joaquin/plot.py
+++ joaquin/plot.py
@@ -1,4 +1,9 @@
+import warnings
+
+import astropy.units as u
+import astropy.coordinates as coord
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
 phot_to_label = {
@@ -82,3 +87,80 @@ def simple_corner(X, labels=None, color_by=None, axes=None,
         return_stuff.append(cb)
 
     return return_stuff
+
+
+def plot_hr_cmd(parent_data, stars,
+                idx0, other_idx,
+                teff_logg_bins=None, cmd_bins=None):
+
+    style_main = dict(ls='none', marker='o', mew=0.6, ms=6.,
+                      color='tab:blue', zorder=100,
+                      mec='gold')
+    style_neighbors = dict(ls='none', marker='o', mew=0, ms=2.,
+                           alpha=0.75, color='tab:orange', zorder=10)
+
+    if teff_logg_bins is None:
+        teff_logg_bins = (
+            np.linspace(3000, 9000, 128),
+            np.linspace(-0.5, 5.75, 128))
+
+    if cmd_bins is None:
+        cmd_bins = (np.linspace(-0.5, 2, 128),
+                    np.linspace(-6, 10, 128))
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    ax = axes[0]
+    ax.hist2d(parent_data.stars['TEFF'], parent_data.stars['LOGG'],
+              bins=teff_logg_bins, norm=mpl.colors.LogNorm(),
+              cmap='Greys')
+
+    ax.plot(stars['TEFF'][idx0],
+            stars['LOGG'][idx0],
+            **style_main)
+
+    ax.plot(stars['TEFF'][other_idx],
+            stars['LOGG'][other_idx],
+            **style_neighbors)
+
+    ax.set_xlim(teff_logg_bins[0].max(),
+                teff_logg_bins[0].min())
+    ax.set_ylim(teff_logg_bins[1].max(),
+                teff_logg_bins[1].min())
+
+    ax.set_xlabel(r'$T_{\rm eff}$')
+    ax.set_ylabel(r'$\log g$')
+
+    # ---
+
+    ax = axes[1]
+
+    color = ('J', 'K')
+    mag = 'H'
+
+    dist_mask, = np.where((parent_data.stars['GAIAEDR3_PARALLAX'] / parent_data.stars['GAIAEDR3_PARALLAX_ERROR']) > 5)
+    distmod = coord.Distance(parallax=parent_data.stars['GAIAEDR3_PARALLAX'][dist_mask]*u.mas).distmod.value
+    ax.hist2d((parent_data.stars[color[0]] - parent_data.stars[color[1]])[dist_mask],
+              parent_data.stars[mag][dist_mask] - distmod,
+              bins=cmd_bins, norm=mpl.colors.LogNorm(),
+              cmap='Greys')
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        distmod = coord.Distance(parallax=stars['GAIAEDR3_PARALLAX']*u.mas,
+                                 allow_negative=True).distmod.value
+    ax.plot((stars[color[0]] - stars[color[1]])[idx0],
+            (stars[mag] - distmod)[idx0],
+            **style_main)
+
+    ax.plot((stars[color[0]] - stars[color[1]])[other_idx],
+            (stars[mag] - distmod)[other_idx],
+            **style_neighbors)
+
+    ax.set_xlim(cmd_bins[0].min(), cmd_bins[0].max())
+    ax.set_ylim(cmd_bins[1].max(), cmd_bins[1].min())
+
+    ax.set_xlabel('$J - K$')
+    ax.set_ylabel('$M_H$')
+
+    return fig
