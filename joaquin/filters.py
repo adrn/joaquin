4142cc87c5be96e6d53a4ce98bf8d65ae7436469
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-04 11:13:10 -0400

    rename stretch func

--- joaquin/filters.py
+++ joaquin/filters.py
@@ -11,22 +11,35 @@ def nufft_lowpass(x, y, fcut, bad_mask=None, **kwargs):
     Nx = len(x)
     minx, maxx = np.min(x), np.max(x)
 
-    fac = (maxx - minx) * (Nx + 1) / Nx / np.pi
+    fac = (maxx - minx) * (Nx + 1) / Nx / np.pi  # TODO: needs justification
 
     scaled_x = (x - minx) / fac
 
     shift_y = np.mean(y[~bad_mask])
     shifted_y = y - shift_y
 
-    Nhalf = np.round(fcut * fac * 2*np.pi)
-    Nf = int(2 * Nhalf + 1)
+    Nhalf = np.round(fcut * fac * 2*np.pi)  # TODO: needs justification
+    Nf = int(2 * Nhalf + 1)  # TODO: needs justification
 
     f = finufft.nufft1d1(scaled_x[~bad_mask],
                          shifted_y[~bad_mask].astype(np.complex128),
                          n_modes=Nf,
                          **kwargs)
 
+    # TODO: needs justification
     inv_fft_y = finufft.nufft1d2(scaled_x, f, **kwargs) / len(scaled_x) / 2
     assert np.all(np.abs(inv_fft_y.imag) < 1e-10)
 
     return inv_fft_y.real + shift_y
+
+
+def renormalize(x, vmin=None, vmax=None):
+    x = np.array(x)
+
+    if vmin is None:
+        vmin = np.min(x)
+
+    if vmax is None:
+        vmax = np.max(x)
+
+    return (x - vmin) / (vmax - vmin)
