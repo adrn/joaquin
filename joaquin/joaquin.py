f8853a66029a10bad9c9c0348a700e5dced882ef
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-13 11:20:03 -0400

    code

--- joaquin/joaquin.py
+++ joaquin/joaquin.py
@@ -10,9 +10,10 @@ from .logger import logger
 
 class Joaquin:
 
-    def __init__(self, data, terms=['phot', 'lsf', 'spec'], frozen=None):
-        self.data = data
-        self.X, self.y, self.y_ivar, self.idx_map = self.data.get_sub_Xy(terms)
+    def __init__(self, X, y, y_ivar, idx_map, frozen=None):
+        self.X = X
+        self.y = y
+        self.y_ivar = y_ivar
 
         # Currently, stores parameter names and shapes
         self._param_info = {}
@@ -26,7 +27,8 @@ class Joaquin:
         # linear coefficients (in the exp argument)
         self._param_info['beta'] = self.X.shape[1]
 
-        if 'spec' in terms:
+        self.idx_map = idx_map
+        if 'spec' in idx_map:
             L2_slice = self.idx_map['spec']
         else:
             L2_slice = np.ones(self.X.shape[1], dtype=bool)
@@ -43,10 +45,7 @@ class Joaquin:
             if key in self.frozen:
                 par_dict[key] = self.frozen[key]
             else:
-                par_dict[key] = np.array(par_list[i:i+par_len])
-                if len(par_dict[key]) == 1:  # HORRIBLE
-                    par_dict[key] = par_dict[key][0]
-
+                par_dict[key] = np.squeeze(par_list[i:i+par_len])
                 i += par_len
 
         return par_dict
@@ -55,10 +54,10 @@ class Joaquin:
         parvec = []
         for i, k in enumerate(self._param_info):
             if k not in self.frozen:
-                parvec.append(par_dict[k])
+                parvec.append(np.atleast_1d(par_dict[k]))
         return np.concatenate(parvec)
 
-    def init_beta(self, parallax_zpt=None, L2_ivar=None):
+    def init_beta(self, parallax_zpt=None, L2_ivar=None, **_):
         parallax_zpt = self.frozen.get('parallax_zpt', parallax_zpt)
         L2_ivar = self.frozen.get('L2_ivar', L2_ivar)
 
@@ -82,34 +81,90 @@ class Joaquin:
         beta = np.linalg.solve(XT_Cinv_X, np.dot(XT_Cinv, ln_y))
         return beta
 
-    def chi(self, parallax_zpt, L2_ivar, beta):
-        y = self.y + parallax_zpt
-        model_ln_plx = np.dot(self.X, beta)
+    def init(self, pack=True, **kwargs):
+        init_dict = kwargs.copy()
+
+        if 'beta' not in self.frozen:
+            init_dict['beta'] = self.init_beta(**kwargs)
+
+        if 'parallaz_zpt' not in self.frozen:
+            init_dict['parallax_zpt'] = kwargs.get(
+                'parallax_zpt', -0.03)  # MAGIC NUMBER (Gaia value)
+
+        if pack:
+            return self.pack_pars(init_dict)
+        else:
+            return init_dict
+
+    @classmethod
+    def model_y(cls, X, *, beta, **_):
+        model_ln_plx = np.dot(X, beta)
         model_y = np.exp(model_ln_plx)
+        return model_y
+
+    def chi(self, *, parallax_zpt, beta, **_):
+        y = self.y + parallax_zpt
+        # model_ln_plx = np.dot(self.X, beta)
+        # model_y = np.exp(model_ln_plx)
+        model_y = self.model_y(self.X, parallax_zpt=parallax_zpt, beta=beta)
         resid = y - model_y
         return resid * np.sqrt(self.y_ivar)
 
-    def ln_likelihood(self, parallax_zpt, L2_ivar, beta):
+    def ln_likelihood(self, *, parallax_zpt, beta, **_):
+        ll_grad = {}
+
         y = self.y + parallax_zpt
-        model_ln_plx = np.dot(self.X, beta)
-        model_y = np.exp(model_ln_plx)
+
+        # model_ln_plx = np.dot(self.X, beta)
+        # model_y = np.exp(model_ln_plx)
+        model_y = self.model_y(self.X, parallax_zpt=parallax_zpt, beta=beta)
         resid = y - model_y
 
         ll = -0.5 * np.sum(resid**2 * self.y_ivar)
-        ll_grad = np.dot(self.X.T,
-                         model_y * self.y_ivar * resid)
+
+        if 'beta' not in self.frozen:
+            ll_grad['beta'] = np.dot(self.X.T,
+                                     model_y * self.y_ivar * resid)
+
+        if 'parallax_zpt' not in self.frozen:
+            ll_grad['parallax_zpt'] = \
+                - np.sum((y + parallax_zpt - model_y) * self.y_ivar)
 
         return ll, ll_grad
 
-    def ln_prior(self, parallax_zpt, L2_ivar, beta):
-        lp = - 0.5 * L2_ivar * np.sum(beta[self.L2_slice] ** 2)
-        lp_grad = np.zeros_like(beta)
-        lp_grad[self.L2_slice] = - L2_ivar * beta[self.L2_slice]
+    def ln_prior(self, *, parallax_zpt, L2_ivar, beta, **_):
+        lp = 0.
+        lp_grad = {}
+
+        if 'beta' not in self.frozen:
+            lp += - 0.5 * L2_ivar * np.sum(beta[self.L2_slice] ** 2)
+            lp_grad['beta'] = np.zeros_like(beta)
+            lp_grad['beta'][self.L2_slice] = - L2_ivar * beta[self.L2_slice]
+
+        if 'parallax_zpt' not in self.frozen:
+            plx_zpt_var = 0.1 ** 2  # MAGIC NUMBER
+            lp += - 0.5 * parallax_zpt ** 2 / plx_zpt_var
+            lp_grad['parallax_zpt'] = -1 / plx_zpt_var * parallax_zpt
+
         return lp, lp_grad
 
     def neg_ln_posterior(self, parallax_zpt, L2_ivar, beta):
-        ll, ll_grad = self.ln_likelihood(parallax_zpt, L2_ivar, beta)
-        lp, lp_grad = self.ln_prior(parallax_zpt, L2_ivar, beta)
+        ll, ll_grad = self.ln_likelihood(
+            parallax_zpt=parallax_zpt,
+            beta=beta)
+
+        lp, lp_grad = self.ln_prior(
+            L2_ivar=L2_ivar,
+            parallax_zpt=parallax_zpt,
+            beta=beta)
+
+        lp_grad = np.concatenate([np.atleast_1d(lp_grad[k])
+                                  for k in self._param_info
+                                  if k not in self.frozen])
+        ll_grad = np.concatenate([np.atleast_1d(ll_grad[k])
+                                  for k in self._param_info
+                                  if k not in self.frozen])
+
         logger.log(0, f'objective function evaluation: ll={ll}, lp={lp}')
         return - (ll + lp), - (ll_grad + lp_grad)
 
