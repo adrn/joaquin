b791bbac6d2764c93d676294d098ab984ed9d2be
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-18 10:44:22 -0400

    add one command: run

--- joaquin/cli/main.py
+++ joaquin/cli/main.py
@@ -33,49 +33,40 @@ class CLI:
 
         getattr(self, args.command)()
 
-    def make_neighborhoods(self):
-        """PCA the spectra to find neighbors and define neighborhoods"""
-        from .make_neighborhoods import make_neighborhoods
+    # def make_neighborhoods(self):
+    #     """PCA the spectra to find neighbors and define neighborhoods"""
+    #     from .make_neighborhoods import make_neighborhoods
+    #     parser = get_parser(
+    #         description=(
+    #             "TODO"),
+    #         multiproc_options=False)
+
+    #     # HACK
+    #     parser.usage = 'joaquin make_neighborhoods' + parser.format_usage()[9:]
+    #     args = parser.parse_args(sys.argv[2:])
+    #     make_neighborhoods(args.config_file)
+
+    def run(self):
+        """Run Joaquin on the pre-generated neighborhoods"""
+        from .run import run_pipeline
+
         parser = get_parser(
             description=(
-                "TODO"),
-            multiproc_options=False)
+                "This command is the main workhorse for Joaquin: it runs the "
+                "distance training and prediction on all of the stars in the "
+                "spectral neighborhoods."),
+            loggers=[logger])
 
         # HACK
-        parser.usage = 'joaquin make_neighborhoods' + parser.format_usage()[9:]
-        args = parser.parse_args(sys.argv[2:])
-        make_neighborhoods(args.config_file)
-
-    def run_thejoker(self):
-        """Run The Joker on the input data"""
-        from thejoker.logging import logger as joker_logger
-        from .run_thejoker import run_thejoker
-
-        parser = get_parser(
-            description=(
-                "This command is the main workhorse for HQ: it runs The Joker "
-                "on all of the input data and caches the samplings."),
-            loggers=[logger, joker_logger])
-        # HACK
-        parser.usage = 'hq run_thejoker' + parser.format_usage()[9:]
+        parser.usage = 'joaquin run' + parser.format_usage()[9:]
 
         parser.add_argument("-s", "--seed", dest="seed", default=None,
                             type=int, help="Random number seed")
-        parser.add_argument("--limit", dest="limit", default=None,
-                            type=int, help="Maximum number of stars to process")
 
         args = parser.parse_args(sys.argv[2:])
 
-        if args.seed is None:
-            args.seed = np.random.randint(2**32 - 1)
-            logger.log(
-                1, f"No random seed specified, so using seed: {args.seed}")
-
-        with threadpool_limits(limits=1, user_api='blas'):
-            with args.Pool(**args.Pool_kwargs) as pool:
-                run_thejoker(run_path=args.run_path, pool=pool,
-                             overwrite=args.overwrite,
-                             seed=args.seed, limit=args.limit)
+        with args.Pool(**args.Pool_kwargs) as pool:
+            run_pipeline(args.config_file, pool=pool)
 
         sys.exit(0)
 
