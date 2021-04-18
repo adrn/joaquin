ea85328cc9266207da946a37fa0c9c0726855ad9
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-18 13:03:29 -0400

    update Kfold func

--- joaquin/crossval.py
+++ joaquin/crossval.py
@@ -1,27 +1,43 @@
 import numpy as np
 
 
-def get_Kfold_indices(N, K, rng=None):
+def get_Kfold_indices(K, train_mask, block_size, rng=None):
 
     if rng is None:
         rng = np.random.default_rng()
 
-    idx = np.arange(N)
-    rng.shuffle(idx)
+    if train_mask.dtype is np.dtype(bool):
+        train_idx = np.argwhere(train_mask).ravel()
+    else:
+        train_idx = train_mask
 
-    batch_size = N // K
+    assert block_size < len(train_mask)
+
+    # Now split into block and zone 2 stars: the K-fold will
+    # only happen on the block stars, and the zone 2 stars
+    # will be appended to all blocks
+    block_idx = train_idx[:block_size].copy()
+    rng.shuffle(block_idx)
+
+    zone2_idx = train_idx[block_size:]
+
+    batch_size = block_size // K
     train_batches = []
     test_batches = []
     for k in range(K):
         if k == K-1:
-            batch = idx[k*batch_size:]
+            test_batch = block_idx[k*batch_size:]
         else:
-            batch = idx[k*batch_size:(k+1)*batch_size]
+            test_batch = block_idx[k*batch_size:(k+1)*batch_size]
 
-        test_batches.append(batch)
-        train_batches.append(idx[~np.isin(idx, batch)])
+        train_batch = np.concatenate(
+            (block_idx[~np.isin(block_idx, test_batch)], zone2_idx))
 
-    assert np.all(np.array([len(train_batches[i]) + len(test_batches[i])
-                            for i in range(len(train_batches))]) == N)
+        test_batches.append(test_batch)
+        train_batches.append(train_batch)
+
+    assert np.all(np.array([
+        len(train_batches[i]) + len(test_batches[i])
+        for i in range(len(train_batches))]) == len(train_idx))
 
     return train_batches, test_batches
